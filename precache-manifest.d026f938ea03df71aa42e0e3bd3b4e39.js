7cdcff6097152fbad118f6f66b4451278db02cf7
deotor <deotor@gmail.com>
2019-03-07 12:20:46 +0200

    Updates

---
+++ precache-manifest.d026f938ea03df71aa42e0e3bd3b4e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d9e9e769cf1e02ba45",
+    "url": "/movie-app-react/static/js/runtime~main.97d9e9e7.js"
+  },
+  {
+    "revision": "414f54f00c2f42743625",
+    "url": "/movie-app-react/static/js/main.414f54f0.chunk.js"
+  },
+  {
+    "revision": "742dd430354f29115d6f",
+    "url": "/movie-app-react/static/js/1.742dd430.chunk.js"
+  },
+  {
+    "revision": "414f54f00c2f42743625",
+    "url": "/movie-app-react/static/css/main.05aaeb42.chunk.css"
+  },
+  {
+    "revision": "742dd430354f29115d6f",
+    "url": "/movie-app-react/static/css/1.cdf36f68.chunk.css"
+  },
+  {
+    "revision": "48b248457a5570ec9df042793a6bec79",
+    "url": "/movie-app-react/index.html"
+  }
+];
